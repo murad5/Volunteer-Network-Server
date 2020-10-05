7748eb395cb0cf2d9cfeac481a69a66a36851f43
murad-aust <murad.rony15@gmail.com>
2020-10-05 11:59:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.us2jj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const port = 5000;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+client.connect(err => {
+  const eventsCollection = client.db("volunteerNetwork").collection("events");
+  const activitiesCollection = client.db("volunteerNetwork").collection("activities");
+  
+    
+  app.post('/addEvents', (req, res) => {
+    const events = req.body;
+  
+    eventsCollection.insertMany(events)
+    .then(result => {
+      console.log(result.insertCount)
+      res.send(result.insertCount)
+    })
+  })
+
+  app.get('/events', (req, res) => {
+    eventsCollection.find({})
+    .toArray((err, documents)=>{
+      res.send(documents);
+    })
+  })
+
+
+  app.get('/event/:id', (req, res) => {
+
+    eventsCollection.find({_id: ObjectId(req.params.id)})
+    .toArray( (err, documents) => {
+      
+        res.send(documents[0]);
+    })
+})
+
+
+
+  app.post('/addActivities', (req, res) => {
+    const newActivity = req.body;
+    activitiesCollection.insertOne(newActivity)
+      .then(result => {
+        res.redirect('/activities');
+      })
+
+  })
+
+  app.get('/activities', (req, res) => {
+    activitiesCollection.find({email: req.query.email})
+    .toArray((err, documents)=>{
+      res.send(documents);
+    })
+  })
+
+  app.delete('/delete/:id',(req, res) =>{
+    activitiesCollection.deleteOne({_id : ObjectId(req.params.id)})
+    .then(result =>{
+      // res.redirect('/activities')
+      res.send(result.deletedCount >0);
+
+    })
+  })
+
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello from volunteer')
+  })
+
+  app.listen(port)
